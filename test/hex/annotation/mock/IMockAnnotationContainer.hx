481e57a4a3e4418ead2a57c5b61201e95843cb4e
francis.bourre <francis_bourre@me.com>
2016-07-21 07:08:49 +0200

    minor fix for macro

--- test/hex/annotation/mock/IMockAnnotationContainer.hx
+++ test/hex/annotation/mock/IMockAnnotationContainer.hx
@@ -3,8 +3,10 @@ package hex.annotation.mock;
 /**
  * @author Francis Bourre
  */
+#if !macro
 @:remove
 @:autoBuild( hex.annotation.AnnotationReader.readMetadata( "hex.annotation.mock.IMockAnnotationContainer", [ "Inject", "Language", "Test", "PostConstruct", "Optional", "ConstructID" ] ) )
+#end
 interface IMockAnnotationContainer
 {
 	
