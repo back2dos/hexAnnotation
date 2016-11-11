cb7c748c89c5e7ecfbe22b89e7bdd3b68dd4512f
francis.bourre <francis_bourre@me.com>
2016-11-11 11:00:20 +0100

    Old annotation system removed. It's been replaced by hexReflection repository.

--- src/hex/annotation/IClassAnnotationDataProvider.hx
+++
@@ -1,9 +0,0 @@
-package hex.annotation;
-
-/**
- * @author Francis Bourre
- */
-interface IClassAnnotationDataProvider 
-{
-	function getClassAnnotationData( type : Class<Dynamic> ) : ClassAnnotationData;
-}
