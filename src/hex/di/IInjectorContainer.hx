516160bed01b1f1dde7c89b20566fff79d3f6569
Francis Bourre <francis_bourre@me.com>
2016-11-13 08:37:56 +0100

    missing class added

---
+++ src/hex/di/IInjectorContainer.hx
@@ -0,0 +1,13 @@
+package hex.di;
+
+/**
+ * @author Francis Bourre
+ */
+#if !macro
+@:remove
+@:autoBuild( hex.di.annotation.FastAnnotationReader.readMetadata( hex.di.IInjectorContainer ) )
+#end
+interface IInjectorContainer
+{
+	
+}
