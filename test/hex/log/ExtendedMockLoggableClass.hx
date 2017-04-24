9445f71356db1a81d1b1c3023739406ece6dbb20
St3veV <stepan.vyterna@gmail.com>
2017-04-24 13:50:05 +0200

    Adding search for loggers

--- test/hex/log/ExtendedMockLoggableClass.hx
+++ test/hex/log/ExtendedMockLoggableClass.hx
@@ -1,4 +1,5 @@
 package hex.log;
+import hex.module.Module;
 
 /**
  * ...
@@ -9,7 +10,64 @@ class ExtendedMockLoggableClass extends MockLoggableClass
 	
 }
 
-private class AnotherExtendedMockLoggableClass extends ExtendedMockLoggableClass
+class AnotherExtendedMockLoggableClass extends ExtendedMockLoggableClass
 {
 	
+}
+
+class MockLoggableClassWithCustomLogger implements IsLoggable
+{
+	public var myLogger:ILogger;
+	
+	public function new() 
+	{
+	}
+	
+	@Debug
+	public function callDebug()
+	{
+		
+	}
+	
+}
+
+class ExtendsLoggableWithCustomLogger extends MockLoggableClassWithCustomLogger
+{
+	
+	@Debug
+	public function callDebugExtend()
+	{
+		
+	}
+}
+
+class ClassWithCustomLogger
+{
+	public var custom:ILogger;
+	
+	public function new() 
+	{
+	}
+	
+}
+
+class MockLoggableWithExtendsCustomLogger extends ClassWithCustomLogger implements IsLoggable
+{
+	
+	@Debug
+	public function callDebugExtend()
+	{
+		
+	}
+}
+
+class ExtendsLoggableAndOverridesLogger extends MockLoggableClassWithCustomLogger
+{
+	public var overriddenLogger:ILogger;
+	
+	@Debug
+	public function callDebugExtend()
+	{
+		
+	}
 }
