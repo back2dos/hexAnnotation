ba510738f2d78b3f37b4ab8a9371addad1311818
Laurent Deketelaere <Laurent.Deketelaere@doclerholding.com>
2017-08-30 14:11:26 +0200

    Add ExitingNotifier due to modification in hexUnit : DoclerLabs/hexMachina#232

--- test/MainAnnotationTest.hx
+++ test/MainAnnotationTest.hx
@@ -20,6 +20,7 @@ class MainAnnotationTest
 		#else
 		emu.addListener( new hex.unittest.notifier.ConsoleNotifier( ) );
 		#end
+		emu.addListener( new hex.unittest.notifier.ExitingNotifier( ) );
 		
         emu.addTest( HexAnnotationSuite );
         emu.run();
